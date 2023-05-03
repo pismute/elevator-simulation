341561e94a7cc8c29bd6d067449f71346a5b2349
Changwoo Park <pismute@gmail.com>
2023-05-03 11:28:19 +0200

    add TestControl for sleep

--- shell/src/test/scala/shell/elevator/ce/CEFloorDoorsSpec.scala
+++ shell/src/test/scala/shell/elevator/ce/CEFloorDoorsSpec.scala
@@ -2,6 +2,7 @@ package shell.elevator.ce
 
 import cats.effect.IO
 import cats.effect.syntax.spawn.*
+import cats.effect.testkit.TestControl
 import cats.syntax.applicative.*
 import cats.syntax.apply.*
 import cats.syntax.flatMap.*
@@ -26,29 +27,33 @@ class CEFloorDoorsSpec extends TestAppSuite:
   }
 
   test("A fiber should await and be awaken: lower boundary") {
-    runAppT(appEnv) {
-      CEInterpreters.floorDoors[AppT](1, 3).use { floorDoors =>
-        for
-          fib <- floorDoors.await(1).start // lower bound
-          // awake must be called after await
-          _   <- IO.sleep(100.milli).liftTo[AppT]
-          _   <- floorDoors.awake(1)
-          _   <- fib.join
-        yield ()
+    TestControl.executeEmbed {
+      runAppT(appEnv) {
+        CEInterpreters.floorDoors[AppT](1, 3).use { floorDoors =>
+          for
+            fib <- floorDoors.await(1).start // lower bound
+            // awake must be called after await
+            _   <- IO.sleep(100.milli).liftTo[AppT]
+            _   <- floorDoors.awake(1)
+            _   <- fib.join
+          yield ()
+        }
       }
     }
   }
 
   test("A fiber should await and be awaken: upper boundary") {
-    runAppT(appEnv) {
-      CEInterpreters.floorDoors[AppT](1, 3).use { floorDoors =>
-        for
-          fib <- floorDoors.await(3).start // upper boundary
-          // awake must be called after await
-          _   <- IO.sleep(100.milli).liftTo[AppT]
-          _   <- floorDoors.awake(3)
-          _   <- fib.join
-        yield ()
+    TestControl.executeEmbed {
+      runAppT(appEnv) {
+        CEInterpreters.floorDoors[AppT](1, 3).use { floorDoors =>
+          for
+            fib <- floorDoors.await(3).start // upper boundary
+            // awake must be called after await
+            _   <- IO.sleep(100.milli).liftTo[AppT]
+            _   <- floorDoors.awake(3)
+            _   <- fib.join
+          yield ()
+        }
       }
     }
   }
